d992b6c13ecc244fe32bf79c9644d0a0f29fd451
BEKA-WEB-DEV <bktessema9@gmail.com>
2024-11-17 23:20:46 +0300

    category

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import MyCarousel from './components/Carousel/Carousel';
+import Catagory from './components/Catagory/Catagory';
 
 function App() {
   return (
     <>
       <Header />
       <MyCarousel />
+      <Catagory />
     </>
   );
 }
