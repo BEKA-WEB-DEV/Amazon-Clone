4e7856c86dcd4b26bd24c8f1a84437b84ceaf50c
BEKA-WEB-DEV <bktessema9@gmail.com>
2024-11-19 18:38:03 +0300

    category routing

--- client/src/components/Catagory/CatagoryCard.jsx
+++ client/src/components/Catagory/CatagoryCard.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import classes from './Catagory.module.css'
+import { Link } from 'react-router-dom'
+
 
 
 function CatagoryCard({data}) {
   return (
     <div className={classes.category}>
 
-        <a href="/">
+        <Link to={`/catagory/${data.name}`}>
             <span>
                 <h2>{data.title}</h2>
             </span>
             <img src={data.imgLink} alt="" />
             <p>shop now</p>
-        </a>
+        </Link>
 
     </div>
   )
